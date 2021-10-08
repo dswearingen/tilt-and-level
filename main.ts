4fc15da582913c2c3e8001eb078e2f58e01e55a6
Dan Swearingen <dan@autistry.com>
2021-10-08 13:51:16 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,11 @@
 let x = 0
 let y = 0
-let z = 0
+let xx = 0
+let yy = 0
 basic.forever(function () {
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
-    if (Math.abs(x) > 32) {
-        basic.showIcon(IconNames.Square)
-    } else if (Math.abs(y) > 32) {
-        basic.showIcon(IconNames.Square)
-    } else {
-        basic.showIcon(IconNames.No)
-    }
-    z = input.acceleration(Dimension.Z)
-    serial.writeNumber(z)
-    serial.writeLine("")
+    xx = 0.005 * x + 2
+    yy = 0.005 * y + 2
+    led.plot(xx, yy)
 })
