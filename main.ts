40b92033bd72ad058ed2379893027a2e83cd58e1
Dan Swearingen <dan@autistry.com>
2021-10-08 14:32:55 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,17 @@ let x = 0
 let y = 0
 let xx = 0
 let yy = 0
+let tyy = 0
+let txx = 0
 basic.forever(function () {
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
-    xx = 0.005 * x + 2
-    yy = 0.005 * y + 2
-    led.plot(xx, yy)
+    xx = Math.trunc(0.005 * x + 2)
+    yy = Math.trunc(0.005 * y + 2)
+    if (yy != tyy || xx != txx) {
+        led.plot(xx, yy)
+        led.unplot(txx, tyy)
+        txx = xx
+        tyy = yy
+    }
 })
